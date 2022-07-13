e62a97d0e203cd7b9d4d0c7ce0470c0c3056de65
lenovo_kvshop <banserukvitalii@gmail.com>
2022-07-13 17:18:19 +0300

    update 1.3.4

--- src/main/java/io/github/vitalij3/JCA/ClashApi.java
+++ src/main/java/io/github/vitalij3/JCA/ClashApi.java
@@ -23,7 +23,7 @@ import java.util.List;
  /**
   * General class for controlling all Clash Of Clans API functional
   * @author Vitalij
-  * @version 1.3.2
+  * @version 1.3.4
  */
 public class ClashApi {
     private final String API_KEY;
@@ -113,73 +113,271 @@ public class ClashApi {
         return true;
     }
 
+     /**
+      * @param playerTag the player tag
+      * @return player object
+      * @throws ClashOfClansApiException is generated if request code equals critical
+      */
     public Player getPlayerStats(String playerTag) throws ClashOfClansApiException {
         String responseBodyString = get("players/[tag]".replace("[tag]", formatTag(playerTag)));
         return GSON.fromJson(responseBodyString, Player.class);
     }
 
+     /**
+      * @return gold pass object
+      * @throws ClashOfClansApiException is generated if request code equals critical
+      */
     public CurrentGoldPass getCurrentGoldPass() throws ClashOfClansApiException {
         return GSON.fromJson(get("goldpass/seasons/current"), CurrentGoldPass.class);
     }
 
+     /**
+      * @return label objects
+      * @throws ClashOfClansApiException is generated if request code equals critical
+      */
     public List<GeneralLabels.Item> getLabels() throws ClashOfClansApiException {
         return GSON.fromJson(get("labels/clans"), GeneralLabels.class).items;
     }
 
-    public List<Location.Item> getLocations() throws ClashOfClansApiException {
-        return GSON.fromJson(get("locations"), Location.class).items;
-    }
+     /**
+      * @param limit the getting limit
+      * @return label objects
+      * @throws ClashOfClansApiException is generated if request code equals critical
+      */
+     public List<GeneralLabels.Item> getLabels(int limit) throws ClashOfClansApiException {
+         return GSON.fromJson(get("labels/clans?limit=[limit]".replace("[limit]", String.valueOf(limit))), GeneralLabels.class).items;
+     }
 
+     /**
+      * @return location objects
+      * @throws ClashOfClansApiException is generated if request code equals critical
+      */
+     public List<Location.Item> getLocations() throws ClashOfClansApiException {
+        return GSON.fromJson(get("locations"), Location.class).items;
+     }
+
+     /**
+      * @param limit the getting limit
+      * @return location objects
+      * @throws ClashOfClansApiException is generated if request code equals critical
+      */
+     public List<Location.Item> getLocations(int limit) throws ClashOfClansApiException {
+         return GSON.fromJson(get("locations?limit=[limit]".replace("[limit]", String.valueOf(limit))), Location.class).items;
+     }
+
+     /**
+      * @param locationId the location id
+      * @return location object
+      * @throws ClashOfClansApiException is generated if location not found or other request errors
+      */
     public OneLocation getLocationById(Long locationId) throws ClashOfClansApiException {
         return GSON.fromJson(get("locations/" + locationId.toString()), OneLocation.class);
     }
 
+     /**
+      * @param locationId the location id
+      * @return ranking player objects
+      * @throws ClashOfClansApiException is generated if request code equals critical
+      */
     public List<RankingsPlayersVersus.Item> getRankingsPlayersVersus(Long locationId) throws ClashOfClansApiException {
         return GSON.fromJson(get("locations/[id]/rankings/players-versus".replace("[id]", locationId.toString())),
                 RankingsPlayersVersus.class).items;
     }
 
+     /**
+      * @param locationId the location id
+      * @param limit the getting limit
+      * @return ranking player objects
+      * @throws ClashOfClansApiException is generated if request code equals critical
+      */
+     public List<RankingsPlayersVersus.Item> getRankingsPlayersVersus(Long locationId, int limit) throws ClashOfClansApiException {
+         return GSON.fromJson(get("locations/[id]/rankings/players-versus?limit=[limit]".replace("[id]", locationId.toString()).replace("[limit]", String.valueOf(limit))),
+                 RankingsPlayersVersus.class).items;
+     }
+
+     /**
+      * @param locationId the location id
+      * @return ranking versus clan objects
+      * @throws ClashOfClansApiException is generated if request code equals critical
+      */
     public List<RankingsClansVersus.Item> getRankingsClanVersus(Long locationId) throws ClashOfClansApiException {
         return GSON.fromJson(get("locations/[location]/rankings/clans-versus".replace("[location]", locationId.toString())), RankingsClansVersus.class).items;
     }
 
+     /**
+      * @param locationId the location id
+      * @param limit the getting limit
+      * @return ranking versus clan objects
+      * @throws ClashOfClansApiException is generated if request code equals critical
+      */
+     public List<RankingsClansVersus.Item> getRankingsClanVersus(Long locationId, int limit) throws ClashOfClansApiException {
+         return GSON.fromJson(get("locations/[location]/rankings/clans-versus?limit=[limit]"
+                 .replace("[location]", locationId.toString())
+                 .replace("[limit]", String.valueOf(limit))), RankingsClansVersus.class).items;
+     }
+
+     /**
+      * @param locationId the location id
+      * @return ranking player objects
+      * @throws ClashOfClansApiException is generated if request code equals critical
+      */
     public List<RankingsPlayers.Item> getRankingPlayers(Long locationId) throws ClashOfClansApiException {
         return GSON.fromJson(get("locations/[location]/rankings/players".replace("[location]", locationId.toString())), RankingsPlayers.class).items;
     }
 
+     /**
+      * @param locationId the location id
+      * @param limit the getting limit
+      * @return ranking player objects
+      * @throws ClashOfClansApiException is generated if request code equals critical
+      */
+     public List<RankingsPlayers.Item> getRankingPlayers(Long locationId, int limit) throws ClashOfClansApiException {
+         return GSON.fromJson(get("locations/[location]/rankings/players?limit=[limit]"
+                 .replace("[location]", locationId.toString())
+                 .replace("[limit]", String.valueOf(limit))), RankingsPlayers.class).items;
+     }
+
+     /**
+      * @param locationId the location id
+      * @return ranking clan objects
+      * @throws ClashOfClansApiException is generated if request code equals critical
+      */
     public List<RankingsClans.Item> getRankingClans(Long locationId) throws ClashOfClansApiException {
         return GSON.fromJson(get("locations/[location]/rankings/clans".replace("[location]", locationId.toString())), RankingsClans.class).items;
     }
 
+     /**
+      * @param locationId the location id
+      * @param limit getting limit
+      * @return ranking clan objects
+      * @throws ClashOfClansApiException is generated if request code equals critical
+      */
+     public List<RankingsClans.Item> getRankingClans(Long locationId, int limit) throws ClashOfClansApiException {
+         return GSON.fromJson(get("locations/[location]/rankings/clans?limit=[limit]"
+                 .replace("[location]", locationId.toString())
+                 .replace("[limit]", String.valueOf(limit))), RankingsClans.class).items;
+     }
+
+     /**
+      * @return league objects
+      * @throws ClashOfClansApiException is generated if request code equals critical
+      */
     public List<Leagues.Item> getLeagues() throws ClashOfClansApiException {
         return GSON.fromJson(get("leagues"), Leagues.class).items;
     }
 
+     /**
+      * @param limit the getting limit
+      * @return league objects
+      * @throws ClashOfClansApiException is generated if request code equals critical
+      */
+     public List<Leagues.Item> getLeagues(int limit) throws ClashOfClansApiException {
+         return GSON.fromJson(get("leagues?limit=[limit]".replace("[limit]", String.valueOf(limit))), Leagues.class).items;
+     }
+
+     /**
+      * Not recommended for use, although the api allows you to specify any league identifier, only one is valid anyway, otherwise, when you specify another league, an error will be generated
+      * @param leagueId the league id
+      * @return league object list
+      * @throws ClashOfClansApiException is generated if request code equals critical
+      */
     @Deprecated
     public List<LeagueSeason.Item> getLeagueSeasons(Long leagueId) throws ClashOfClansApiException {
         return GSON.fromJson(get("leagues/[id]/seasons".replace("[id]", leagueId.toString())), LeagueSeason.class).items;
     }
 
+     /**
+      * Not recommended for use, although the api allows you to specify any league identifier, only one is valid anyway, otherwise, when you specify another league, an error will be generated
+      * @param leagueId the league id
+      * @param limit the getting limit
+      * @return league objects list
+      * @throws ClashOfClansApiException is generated if request code equals critical
+      */
+     @Deprecated
+     public List<LeagueSeason.Item> getLeagueSeasons(Long leagueId, int limit) throws ClashOfClansApiException {
+         return GSON.fromJson(get("leagues/[id]/seasons?limit=[limit]"
+                 .replace("[id]", leagueId.toString())
+                 .replace("[limit]", String.valueOf(limit))), LeagueSeason.class).items;
+     }
+
+     /**
+      * @return league season objects
+      * @throws ClashOfClansApiException is generated if request code equals critical
+      */
     public List<LeagueSeason.Item> getLeagueSeasons() throws ClashOfClansApiException {
         return getLeagueSeasons(29000022L);
     }
 
+     /**
+      * @param limit the getting limit
+      * @return league season objects
+      * @throws ClashOfClansApiException is generated if request code equals critical
+      */
+     public List<LeagueSeason.Item> getLeagueSeasons(int limit) throws ClashOfClansApiException {
+         return getLeagueSeasons(29000022L, limit);
+     }
+
+     /**
+      * @param leagueId the league id
+      * @return league season object
+      * @throws ClashOfClansApiException is generated if request code equals critical
+      */
     public LeagueSeason.Item getLeagueInfo(Long leagueId) throws ClashOfClansApiException {
         return GSON.fromJson(get("leagues/[id]".replace("[id]", leagueId.toString())), LeagueSeason.Item.class);
     }
 
+     /**
+      * @return war league objects
+      * @throws ClashOfClansApiException is generated if request code equals critical
+      */
     public List<WarLeague.Item> getWarLeagues() throws ClashOfClansApiException {
         return GSON.fromJson(get("warleagues"), WarLeague.class).items;
     }
 
+     /**
+      * @param limit the getting limit
+      * @return wae league objects
+      * @throws ClashOfClansApiException is generated if request code equals critical
+      */
+     public List<WarLeague.Item> getWarLeagues(int limit) throws ClashOfClansApiException {
+         return GSON.fromJson(get("warleagues?limit=[limit]".replace("[limit]", String.valueOf(limit))), WarLeague.class).items;
+     }
+
+     /**
+      * @param warLeagueId the war league id
+      * @return war league object
+      * @throws ClashOfClansApiException is generated if request code equals critical
+      */
     public WarLeague.Item getWarLeagueInfo(@NotNull Long warLeagueId) throws ClashOfClansApiException {
         return GSON.fromJson(get("warleagues/[id]".replace("[id]", warLeagueId.toString())), WarLeague.Item.class);
     }
 
+     /**
+      * @param clanTag the clan tag
+      * @return clan members
+      * @throws ClashOfClansApiException is generated if request code equals critical
+      */
     public List<ClanMembers.Item> getClanMembers(String clanTag) throws ClashOfClansApiException {
         return GSON.fromJson(get("clans/[tag]/members".replace("[tag]", formatTag(clanTag))), ClanMembers.class).items;
     }
 
+     /**
+      * @param clanTag the clan tag
+      * @param limit the getting limit
+      * @return clan members
+      * @throws ClashOfClansApiException is generated if request code equals critical
+      */
+     public List<ClanMembers.Item> getClanMembers(String clanTag, int limit) throws ClashOfClansApiException {
+         return GSON.fromJson(get("clans/[tag]/members?limit=[limit]"
+                 .replace("[tag]", formatTag(clanTag))
+                 .replace("[limit]", String.valueOf(limit))), ClanMembers.class).items;
+     }
+
+     /**
+      * @param clanTag the clan tag
+      * @return clan information
+      * @throws ClashOfClansApiException is generated if request code equals critical
+      */
     public ClanInfo.Item getClanInfo(String clanTag) throws ClashOfClansApiException {
         return GSON.fromJson(get("clans/[tag]".replace("[tag]", formatTag(clanTag))), ClanInfo.Item.class);
     }
